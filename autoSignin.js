d6f36b622dc6984d595dec6e85c262d52b15cfae
星峰 <34774269+gavinliuisme@users.noreply.github.com>
2023-04-23 21:25:35 +0800

    新增签到领取奖励接口

--- autoSignin.js
+++ autoSignin.js
@@ -7,7 +7,8 @@ const { initInstance, getEnv, updateCkEnv } = require('./qlApi.js')
 const notify = require('./sendNotify')
 
 const updateAccesssTokenURL = 'https://auth.aliyundrive.com/v2/account/token'
-const signinURL = 'https://member.aliyundrive.com/v1/activity/sign_in_list'
+const signinURL = 'https://member.aliyundrive.com/v1/activity/sign_in_list?_rx-s=mobile'
+const rewardURL = 'https://member.aliyundrive.com/v1/activity/sign_in_reward?_rx-s=mobile'
 const getdeviceidurl = 'https://api.aliyundrive.com/adrive/v2/user/get'
 const getfilelistURL = 'https://api.aliyundrive.com/adrive/v3/file/list'
 const batchURL = 'https://api.aliyundrive.com/v2/batch'
@@ -51,24 +52,26 @@ function updateAccesssToken(queryBody, remarks, time) {
     })
 }
 
-//签到
-function sign_in(queryBody, access_token, remarks, times) {
+//签到列表
+function sign_in(access_token, remarks, times) {
   const _times = times | 0
   const sendMessage = [remarks]
   return axios(signinURL, {
     method: 'POST',
-    data: queryBody,
+    data: {
+      isReward: false
+    },
     headers: {
-      Authorization: 'Bearer ' + access_token,
+      Authorization: access_token,
       'Content-Type': 'application/json'
     }
   })
     .then(d => d.data)
-    .then(json => {
+    .then(async json => {
       if (!json.success) {
-        sendMessage.push('签到失败')
+        sendMessage.push('签到失败', json.message)
         if(_times<restarttime){
-          return sign_in(queryBody, access_token, remarks,_times+1)
+          return sign_in(access_token, remarks, _times+1)
         }
         else{
           return Promise.reject(sendMessage.join(', '))
@@ -79,28 +82,34 @@ function sign_in(queryBody, access_token, remarks, times) {
 
       const { signInLogs, signInCount } = json.result
       const currentSignInfo = signInLogs[signInCount - 1] // 当天签到信息
+
       sendMessage.push(`本月累计签到 ${signInCount} 天`)
 
-      // 当天签到是否有奖励
-      if (
-        currentSignInfo.reward &&
-        (currentSignInfo.reward.name || currentSignInfo.reward.description )
-      ){
+      // 未领取奖励列表
+      const rewards = signInLogs.filter(
+        v => v.status === 'normal' && !v.isReward
+      )
+
+      if (rewards.length) {
+        for await (reward of rewards) {
+          const signInDay = reward.day
+          try {
+            const rewardInfo = await getReward(access_token, signInDay)
+            sendMessage.push(
+              `第${signInDay}天奖励领取成功: 获得${rewardInfo.name || ''}${
+                rewardInfo.description || ''
+              }`
+            )
+          } catch (e) {
+            sendMessage.push(`第${signInDay}天奖励领取失败:`, e)
+          }
+        }
+      } else if (currentSignInfo.isReward) {
         sendMessage.push(
-          `本次签到获得${currentSignInfo.reward.name || ''}${currentSignInfo.reward.description || ''
+          `今日签到获得${currentSignInfo.reward.name || ''}${
+            currentSignInfo.reward.description || ''
           }`
         )
-      }
-      let res = signInLogs.filter((signInLog) => {
-         return signInLog.status== 'normal'&&!signInLog.isReward&&signInLog.type=="postpone";
-      });
-      if(res.length>0){
-        let days=0
-        res.forEach(function(item,index,self){
-          days+=item.rewardAmount
-        })
-        sendMessage.push(`有${res.length}张容量延期卡共${days}天【待领取,${signInLogs.length
-        -signInCount}天后过期】`)
       }
 
       return sendMessage.join(', ')
@@ -109,11 +118,37 @@ function sign_in(queryBody, access_token, remarks, times) {
       sendMessage.push('签到失败')
       sendMessage.push(e.message)
         if(_times<restarttime){
-          return sign_in(queryBody, access_token, remarks,_times+1)
+          return sign_in(access_token, remarks, _times+1)
         }
         else{
           return Promise.reject(sendMessage.join(', '))
         }
+    })
+}
+
+// 领取奖励
+function getReward(access_token, signInDay, times) {
+  const _times = times | 0
+  return axios(rewardURL, {
+    method: 'POST',
+    data: { signInDay },
+    headers: {
+      authorization: access_token,
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(d => d.data)
+    .then(json => {
+      if (!json.success) {
+        if(_times<restarttime){
+          return getReward(access_token, signInDay, _times+1)
+        }
+        else{
+          return Promise.reject(json.message)
+        }
+      }
+
+      return json.result
     })
 }
 
@@ -300,7 +335,7 @@ async function getRefreshToken() {
         await updateCkEnv(instance, params)
       }
 
-      const sendMessage = await sign_in(queryBody, access_token, remarks)
+      const sendMessage = await sign_in(access_token, remarks)
       console.log(sendMessage)
       console.log('\n')
       message.push(sendMessage)
